9e6dda2ab5645ad72721579bd251e012ee9f6894
krishnankonda <candyshopwar20>
2022-09-30 00:53:41 -0500

    Krishna Development commit

--- src/game.py
+++ src/game.py
@@ -1,22 +1,29 @@
+"""Initialize Pygame"""
+# pylint: disable=no-member
 import pygame
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
 #Initialize Pygame
-pygame.init()
+def initialize():
+    """Initialize Method"""
+    pygame.init()
 
-#Create Display
-screen = pygame.display.set_mode((800,600))
+    #Create Display
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
-#Title and Icon
-pygame.display.set_caption("BeatDrop")
-icon = pygame.image.load('assets/gameicon.png')
-pygame.display.set_icon(icon)
+    #Title and Icon
+    pygame.display.set_caption("BeatDrop")
+    icon = pygame.image.load('assets/gameicon.png')
+    pygame.display.set_icon(icon)
 
-#Default Game Loop
-RUNNING = True
-while RUNNING:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            RUNNING = False
-    screen.fill((75, 0, 130))
-    pygame.display.update()
-    
+    #Default Game Loop
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((75, 0, 130))
+        pygame.display.update()
+        
--- tests/example-test.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-@pytest.fixture
-def example_fixture():
-    return 1 + 1
-
-def test_with_fixture(example_fixture):
-    assert example_fixture == 2
